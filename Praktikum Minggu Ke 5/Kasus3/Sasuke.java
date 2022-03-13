b1b46f01b1bf5970073131b95798e14933377593
muhfadhb <99516676+muhfadhb@users.noreply.github.com>
2022-03-13 22:58:24 +0700

    Delete Sasuke.java

--- Praktikum Minggu Ke 5/Kasus3/Sasuke.java
+++
@@ -1,9 +0,0 @@
-public class Sasuke extends Itachi{
-	String Dojutsu = "Sharingan";
-	Itachi i = new Itachi();
-	
-	void printDojutsu() {
-		i.printDojutsu();
-		System.out.println(this.Dojutsu);
-	}
-}
