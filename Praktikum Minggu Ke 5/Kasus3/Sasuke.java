93ed66ef9a420beef99bdc41a53b7db8ecf4df02
muhfadhb <99516676+muhfadhb@users.noreply.github.com>
2022-03-13 22:01:41 +0700

    Add files via upload

---
+++ Praktikum Minggu Ke 5/Kasus3/Sasuke.java
@@ -0,0 +1,9 @@
+public class Sasuke extends Itachi{
+	String Dojutsu = "Sharingan";
+	Itachi i = new Itachi();
+	
+	void printDojutsu() {
+		i.printDojutsu();
+		System.out.println(this.Dojutsu);
+	}
+}
